2f9f8b59bbad5423a51abf84fca656e7b7799e12
nabenitez <nabenitez@uc.cl>
2024-10-03 07:47:40 -0300

    chore(deps): add base app code

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Footer } from "@/components/footer";
+import { Container } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Container maxWidth="xl">Base app</Container>
+    </>
+  );
+}
