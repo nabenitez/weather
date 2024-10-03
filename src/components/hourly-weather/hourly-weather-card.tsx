7b46901a7003878dcafdb8f6beab59fb5a5462dc
nabenitez <nabenitez@uc.cl>
2024-10-03 12:59:11 -0300

    feat(hourly-weather): add card

---
+++ src/components/hourly-weather/hourly-weather-card.tsx
@@ -0,0 +1,41 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+
+interface HourlyWeatherCardProps {
+  time: string;
+  temperature: number;
+  pop: number;
+  icon: string;
+}
+
+export const HourlyWeatherCard = ({
+  time,
+  temperature,
+  pop,
+  icon,
+}: HourlyWeatherCardProps) => {
+  const imgUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: 2,
+        width: 100,
+      }}
+    >
+      <Typography variant="h5" component="div">
+        {temperature}°
+      </Typography>
+      <Typography variant="subtitle1" color="info.main">
+        {pop}%
+      </Typography>
+      <Image src={imgUrl} alt="weather icon" width={50} height={50} />
+      <Typography variant="body2" color="textSecondary">
+        {time}
+      </Typography>
+    </Box>
+  );
+};
