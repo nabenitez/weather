e1b9186d7c7be9039e5f8bfecc65a47810e119a7
nabenitez <nabenitez@uc.cl>
2024-10-04 08:37:37 -0300

    feat(app): enable query client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
-import * as React from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
+"use client";
+
+import * as React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang="en">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {props.children}
+            <QueryClientProvider client={queryClient}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              {props.children}
+            </QueryClientProvider>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
