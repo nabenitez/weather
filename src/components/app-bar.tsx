7081f17a2348ff4f79fde10dba12e6dbf4843155
nabenitez <nabenitez@uc.cl>
2024-10-04 08:39:44 -0300

    feat(app-bar): add query invalidation

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,9 +1,13 @@
+"use client";
 import { Toolbar, AppBar as MUIAppBar, Typography } from "@mui/material";
 import { SearchButton } from "./search-button";
+import RefreshButton from "./refresh-button";
+import { useQueryClient } from "@tanstack/react-query";
 
 export function AppBar() {
   // TODO: use internazionalization library such as i18next
   const locales = { title: "Simple Weather" };
+  const queryClient = useQueryClient();
 
   return (
     <MUIAppBar position="static" sx={{ top: "auto", bottom: 0 }}>
@@ -12,6 +16,15 @@ export function AppBar() {
           {locales.title}
         </Typography>
         <SearchButton />
+        <RefreshButton
+          onClick={() => {
+            console.log("invalidating query");
+            queryClient.invalidateQueries({
+              queryKey: ["weatherByCity"],
+              exact: false,
+            });
+          }}
+        />
       </Toolbar>
     </MUIAppBar>
   );
