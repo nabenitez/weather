e13af14faa55e4f426bc579de91478808684a9f7
nabenitez <nabenitez@uc.cl>
2024-10-04 08:44:36 -0300

    feat(app): add weather feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+"use client";
 import * as React from "react";
 import { Footer } from "@/components/footer";
 import { Container } from "@mui/material";
 import { AppBar } from "@/components/app-bar";
+import { Weather } from "./weather";
 
 export default function Home() {
+  const [lastUpdatedAt, setLastUpdatedAt] = React.useState<Date | null>(null);
+
   return (
     <main>
       <AppBar />
-      <Container maxWidth="xl"></Container>
-      <Footer updatedAt={new Date()} />
+      <Container maxWidth="lg">
+        <Weather setLastUpdatedAt={setLastUpdatedAt} />
+      </Container>
+      <Footer updatedAt={lastUpdatedAt} />
     </main>
   );
 }
